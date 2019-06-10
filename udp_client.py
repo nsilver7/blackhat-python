ff1ff46fd157f6acabb85c1918ce1d8d4671fd70
nsilver7 <nicksilver@gmail.com>
2019-06-09 21:20:09 -0500

    adding udp client

---
+++ udp_client.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send some data
+client.sendto(b"AABBCCDD", (target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+print(data)
